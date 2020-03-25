5b593b6cf8d0dff69650b1277ee80ddc0bb9bb33
WINN <winn.voravuthikunchai@telenor.com>
2020-03-25 22:49:09 +0700

    first commit

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from linebot import (
 
 app = Flask(__name__)
 
-lineaccesstoken = 'sQzN2xRRpN4/dLWZoEo5kzM4CfArxSV9gKu2aZFzP0CXN/ODLfuKbzIaKCnmoP16HDhRXDGiTyBQTDfq8S8KHfOJbDW8aBCylHAYg+xjZElZHwdkZ7z1Mp6S7tYH1ipAfFwVTl3bnuuhczKYYnvFfQdB04t89/1O/w1cDnyilFU='
+lineaccesstoken = 'sVi63a9na79V/b+yduVI4yExkvFLcsZGHpCNgiDRnJVNdlWG22i5ICYiEfSmyX3o0ES4ZZ268XYaGETDPlSEu6htUND4nMeICcbHDvUoj3JHlLO0ZQBLh26jgoWOOk6moTB3eRp8U0+wBHbt54SZQwdB04t89/1O/w1cDnyilFU='
 line_bot_api = LineBotApi(lineaccesstoken)
 
 ####################### new ########################
